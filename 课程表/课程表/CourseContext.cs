8d1ed80e4ba0b56c92cd51ba7945d8cd74cb42dd
zengxuezhen <alex.zeng.xuezhen@gmail.com>
2015-01-05 21:04:50 +0000

    12
    12

---
+++ 课程表/课程表/CourseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Data.Linq;
+
+namespace TimeTable
+{
+    public class CourseContext:DataContext
+    {
+        //连接字符串，必须设置明确的格式isostore:/
+        public static string Connectionstr = "Data Source=isostore:/Course.sdf";
+        //传递连接字符串到Context基类
+        public CourseContext(string connectionstring) : base(connectionstring) {  }
+        //定义一个在数据库中的表
+        public Table<CourseTable> course;
+    }
+}
---
+++ 课程表/课程表/CoursesCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+namespace TimeTable
+{
+    public class CoursesCollection:INotifyPropertyChanged
+    {
+        private ObservableCollection<CourseTable> coursetables;
+
+        public ObservableCollection<CourseTable> Coursetables
+        {
+            get { return coursetables; }
+            set {
+                if (coursetables!=value)
+                {
+                    coursetables = value;
+                    NotifyPropertyChanged("");
+                }
+               }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+  
+
+}
